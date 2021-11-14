83ae85a156464519572ce040e55ae9442a566b6d
William Park <l.jimenez.leon97@gmail.com>
2021-11-14 14:50:21 -0600

    sexot

--- src/components/peliculas/Pelicula.js
+++ src/components/peliculas/Pelicula.js
@@ -23,7 +23,7 @@ export const Pelicula = ({ history }) => {
             </div>
 
             <div className="pelicula__contenedor">
-                <iframe title={ pelicula.nombreURL } src={ pelicula.url } frameBorder="0" scrolling="no" allowFullScreen sandbox="allow-scripts"></iframe>
+                <iframe title={ pelicula.nombreURL } src={ pelicula.url } frameBorder="0" scrolling="no" allowFullScreen></iframe>
             </div>
         </section>
     )
